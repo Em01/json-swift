dbce402192262bfec3cc105fbfb63dcee0e41147
Em01 <emma_williams1@hotmail.co.uk>
2015-04-27 16:50:31 +0100

    looking at cocoapods for swift

---
+++ TopApps/Example-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  Example-Bridging-Header.h
+//  TopApps
+//
+//  Created by emma williams on 27/04/2015.
+//  Copyright (c) 2015 RayWenderlich. All rights reserved.
+//
+
+#ifndef TopApps_Example_Bridging_Header_h
+#define TopApps_Example_Bridging_Header_h
+
+
+#endif
