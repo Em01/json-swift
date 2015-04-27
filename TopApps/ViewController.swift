3918aed58015316c53466b3925487da9ad07d28b
Em01 <emma_williams1@hotmail.co.uk>
2015-04-27 16:16:35 +0100

    added swifty json

--- TopApps/ViewController.swift
+++ TopApps/ViewController.swift
@@ -11,35 +11,18 @@ import UIKit
 
 class ViewController: UIViewController {
   
-  override func viewDidLoad() {
-    super.viewDidLoad()    
-  }
-
-DataManager.getTopAppsDataFromFileWithSuccess { (data) -> Void in
-    // Get the number 1 app using optional binding and NSJSONSerialization
-    //1
-        var parseError: NSError?
-        let parsedObject: AnyObject? = NSJSONSerialization.JSONObjectWithData(data,
-            options: NSJSONReadingOptions.AllowFragments,
-            error:&parseError)
-    
-        //2
-        if let topApps = parsedObject as? NSDictionary {
-            if let feed = topApps["feed"] as? NSDictionary {
-                if let apps = feed["entry"] as? NSArray {
-                    if let firstApp = apps[0] as? NSDictionary {
-                        if let imname = firstApp["im:name"] as? NSDictionary {
-                            if let appName = imname["label"] as? NSString {
-                                //3
-                                println("Optional Binding: \(appName)")
-                            }
-                        }
-                    }
-                }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        DataManager.getTopAppsDataFromFileWithSuccess { (data) -> Void in
+            // Get #1 app name using SwiftyJSON
+            let json = JSON(data: data)
+            if let appName = json["feed"]["entry"][0]["im:name"]["label"].string {
+                println("SwiftyJSON: \(appName)")
             }
         }
-    }
 }
+
 
 
 
